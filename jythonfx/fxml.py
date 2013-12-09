006bac9155beefcbfe2f0228bb994c2471248d53
Jeremi Biernacki <jeremi360@gmail.com>
2013-12-09 11:58:35 +0100

    + ‘.settings/’ + ‘.settings/org.eclipse.core.resources.prefs’ / ‘jythonfx/fxml$py.class’ / ‘jythonfx/fxml.py’

--- jythonfx/fxml.py
+++ jythonfx/fxml.py
@@ -4,33 +4,21 @@ from javafx.fxml import FXMLLoader as FxmlL
 
 
 class FXMLLoader(AnchorPane):
-    def __init__(self, fxmlfile, ff = 2):
+    def __init__(self, fxmlfile):
         fxml = FxmlL(self.getClass().getResource(fxmlfile))
         fxml.setController(self)
         root = fxml.load()
         self.getChildren().add(root)
         self.root = root
-        self.setIds(self.root, ff)
+        self.setIds(self.root)
 
-
-    def fixFontSize(self, c, ff = 1):
-        try:
-            f = c.getFont()
-            s = f.getSize()*ff
-            f = f.font(s)
-            c.setFont(f)
-            print "set", c.getId(), "font size to", str(s)
-        except:
-            pass
-
-    def setIds(self, children, ff = 1, prefix = ""):
+    def setIds(self, children, prefix = ""):
         for c in children.getChildren():
             try:
-                self.fixFontSize(c, ff)
                 setattr(self, prefix + c.getId(), c)
                 print "self." + prefix + c.getId()
                 try:
-                    self.setIds(c, ff, c.getId() + "_")
+                    self.setIds(c, c.getId() + "_")
                 except:
                     pass
             except:
