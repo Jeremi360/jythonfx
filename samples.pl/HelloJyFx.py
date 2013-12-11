1502674eb720e95d02b10738823b767d38607316
Jeremi Biernacki <jeremi360@gmail.com>
2013-12-11 12:34:19 +0100

    / ‘samples.pl/HelloJyFx.py’

--- samples.pl/HelloJyFx.py
+++ samples.pl/HelloJyFx.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 *-*
 
-import os
 import sys
-sys.path.append(os.path(".."))
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from jythonfx.application import Application
 from javafx.scene import *
