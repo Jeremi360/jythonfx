7b8f7282cb93dd4a255a9fc78104b591b96f3596
Jeremi Biernacki <jeremi360@gmail.com>
2013-12-26 15:56:39 +0100

    fixed fxml

--- jythonfx/fxml.py
+++ jythonfx/fxml.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 *-*
 import fix
 fix.getJavaFX()
-from javafx.scene.layout import AnchorPane
 from javafx.fxml import FXMLLoader as FxmlL
 
 
-class FXMLLoader(AnchorPane):
+class FXMLLoader(object):
     def __init__(self, fxmlfile):
         fxml = FxmlL(self.getClass().getResource(fxmlfile))
         fxml.setController(self)
-        root = fxml.load()
-        self.getChildren().add(root)
-        self.root = root
-        self.setIds(self.root)
+
+        self.body = fxml.load()
+        self.getChildren().add(self.body)
+        self.setIds(self.body)
+
 
     def setIds(self, children, prefix = ""):
         for c in children.getChildren():
