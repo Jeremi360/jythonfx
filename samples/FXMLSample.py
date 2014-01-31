aed3689906ad08c40c195a6c5044c32d0fca3ca3
jeremi360 <jeremi360@gmail.com>
2014-01-31 15:17:50 +0100

    / ‘samples/FXMLSample.py’

--- samples/FXMLSample.py
+++ samples/FXMLSample.py
@@ -1,25 +1,30 @@
 # -*- coding: utf-8 *-*
 #TODO: license & copyrights
 
-from screens import Menu
-from res.values.string import window_title
-
 from jythonfx.application import Application
 from javafx.scene import Scene
+from jythonfx.event import EventHandler as EH
+from jythonfx.fxml import FXMLLoader
+from javafx.scene.layout import AnchorPane
 
-
-
-class GameFX(Application):
+class Layout(FXMLLoader, AnchorPane):
     def __init__(self):
-        self.title = window_title
+        super(Layout, self).__init__("/FXMLSample.fxml")
+
+
+    def OnClick(self, event):
+        print "Hello in FXML World!"
+
+class Sample(Application):
+    def __init__(self):
+        self.title = "FXML Sample"
 
     def start(self, stage):
         stage.setTitle(self.title)
 
-        m = Menu()
-        stage.setScene(Scene(m))
+        stage.setScene(Scene(Layout()))
 
         stage.show()
 
 if __name__ == "__main__":
-    Application.launch(GameFX)
+    Application.launch(Sample)
