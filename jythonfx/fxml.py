2b35fbdd16e63e58cef4ab397596ca0ddf84aa94
jeremi360 <jeremi360@gmail.com>
2014-02-12 15:29:48 +0100

    / ‘jythonfx/fxml.py’

--- jythonfx/fxml.py
+++ jythonfx/fxml.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 *-*
 import fix
+from test.test_isinstance import Child
 fix.getJavaFX()
 from javafx.fxml import FXMLLoader as FxmlL
 from java.io import File
@@ -15,21 +16,31 @@ class FXMLLoader(object):
         self.getChildren().add(self.body)
         self.setIds(self.body)
 
+    def inSetIds(self, c, prefix = ""):
+        if c.getId != "":
+            setattr(self, prefix + c.getId(), c)
+            print "self." + prefix + c.getId()
 
-    def setIds(self, children, prefix = ""):
-        for c in children.getChildren():
             try:
-                if c.getId != "":
-                    setattr(self, prefix + c.getId(), c)
-                    print "self." + prefix + c.getId()
-
-                try:
-                    if c.getChildren() != []:
-                        self.setIds(c, c.getId())
-                except:
-                    pass
+                if c.getChildren() != []:
+                    self.setIds(c, c.getId())
 
             except:
                 pass
+
+    def setIds(self, children, prefix = ""):
+        try:
+            for c in children.getChildren():
+                self.inSetIds(c, prefix)
+
+        except:
+            try:
+                for c in children.getItems():
+                    self.inSetIds(c, prefix)
+
+            except:
+                pass
+
+
 
 
