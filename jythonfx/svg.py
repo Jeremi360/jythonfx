e27a638834b35be80afa60b17080c9f9ff54229f
Jeremi Biernacki <jeremi360@gmail.com>
2014-07-15 09:01:53 +0200

    / ‘jythonfx/svg.py’

--- jythonfx/svg.py
+++ jythonfx/svg.py
@@ -18,7 +18,7 @@ class SVGLoader(Group):
         ids = [path.getAttribute('id') for path
                         in self.doc.getElementsByTagName('path')]
 
-        for i in leg(ids):
+        for i in len(ids):
             ids[i].replace(" ", "_")
             print ids[i]
 
