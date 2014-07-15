da1dcc3f5e7ad8f4371620ec0a578fe454d6704b
Jeremi Biernacki <jeremi360@gmail.com>
2014-07-15 09:00:47 +0200

    / ‘jythonfx/svg.py’

--- jythonfx/svg.py
+++ jythonfx/svg.py
@@ -6,9 +6,11 @@ import os
 class SVGLoader(Group):
     def __init__(self, svg_file):
         self.doc = minidom.parse(svg_file)  # parseString also exists
-        test = self._set_paths()
+        self._set_paths()
         self.doc.unlink()
-        #print test
+
+    def _set_layers(self):
+        layers =
 
     def _set_paths(self, prefix = ""):
         paths = [path.getAttribute('d') for path
@@ -16,9 +18,14 @@ class SVGLoader(Group):
         ids = [path.getAttribute('id') for path
                         in self.doc.getElementsByTagName('path')]
 
+        for i is leg(ids):
+            dis[i].replace(" ", "_")
+            print dis[i]
+
         for d, p in ids, paths:
             Path = shape.SVGPath(p)
             setattr(self,  prefix + d, Path)
+            self.getChildren().add(Path)
             print "self." + prefix + d
 
 
