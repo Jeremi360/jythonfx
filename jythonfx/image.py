8884dd6aac4e1d2c8510f7b5a083ee78a34f132c
Jeremi Biernacki <jeremi360@gmail.com>
2014-07-18 19:01:20 +0200

    / ‘jythonfx/image.py’

--- jythonfx/image.py
+++ jythonfx/image.py
@@ -3,10 +3,10 @@ import fix
 fix.getJavaFX()
 
 from java.io import File
-from javafx.scene.image import Image, ImageView as IV
+from javafx.scene.image import Image as Ifx, ImageView as IV
 
 class Image(IV):
     def __init__(self, path_to_img):
         f = File(path_to_img).toURI().toString()
-        img = Image(f)
+        img = Ifx(f)
         super(Image, self).__init__(img)
